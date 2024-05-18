e6695b91d31fed87aa9c02640bd81ce3d9d8a5b0
LailaCalderon <lailacalderonramos@icloud.com>
2024-05-18 17:20:57 -0600

    first commit

---
+++ resources/js/routes.js
@@ -0,0 +1,34 @@
+import Dashboard from "./components/Dashboard.vue";
+import ListAppointments from "./pages/appointments/ListAppointments.vue";
+import UpdateProfile from "./pages/profile/UpdateProfile.vue";
+import UpdateSettings from "./pages/settings/UpdateSettings.vue";
+import UserList from "./pages/users/UserList.vue";
+
+
+export default [
+    {
+        path: "/admin/dashboard",
+        name: "admin.dashboard",
+        component: Dashboard,
+    },
+    {
+        path: "/admin/appointments",
+        name: "admin.appointments",
+        component: ListAppointments,
+    },
+    {
+        path: "/admin/users",
+        name: "admin.users",
+        component: UserList,
+    },
+    {
+        path: "/admin/settings",
+        name: "admin.settings",
+        component:UpdateSettings,
+    },
+    {
+        path: "/admin/profile",
+        name: "admin.profile",
+        component: UpdateProfile,
+    },
+];
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ApplicationController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/api/users', [UserController::class, 'index']);
+Route::post('/api/users', [UserController::class, 'store']);
+Route::put('/api/users/{user}', [UserController::class, 'update']);
+Route::delete('/api/users/{user}', [UserController::class, 'destroy']);
+
+Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
